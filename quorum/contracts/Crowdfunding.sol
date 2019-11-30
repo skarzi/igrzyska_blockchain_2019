7ec6f7bf4839f5122a1621f75159602dabd9812e
Mc <phenom.home@gmail.com>
2019-11-30 22:23:09 +0100

    Add tests.

--- quorum/contracts/Crowdfunding.sol
+++ quorum/contracts/Crowdfunding.sol
@@ -10,12 +10,21 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 contract Crowdfunding is Ownable {
     address[] public brokers;
     mapping(address => uint256) public brokerMap;
+    function getBrokersCount() public view returns (uint256) {
+        return brokers.length;
+    }
 
     address[] public organizations;
     mapping(address => uint256) public organizationWhitelist;
+    function getOrganizationsCount() public view returns (uint256) {
+        return organizations.length;
+    }
 
     address[] public users;
     mapping(address => uint256) public userWhitelist;
+    function getUsersCount() public view returns (uint256) {
+        return users.length;
+    }
 
     constructor() public
     Ownable() {}
@@ -23,7 +32,7 @@ contract Crowdfunding is Ownable {
     function addBroker(address _broker)
     onlyOwner() public {
         if (brokerMap[_broker] == 0) {
-            brokerMap[_broker] = brokers.length;
+            brokerMap[_broker] = brokers.length + 1;
             brokers.push(_broker);
         } else {
             revert('Already a broker');
@@ -32,7 +41,7 @@ contract Crowdfunding is Ownable {
 
     function removeBroker(address _broker) public {
         if (brokerMap[_broker] != 0) {
-            delete brokers[brokerMap[_broker]];
+            delete brokers[brokerMap[_broker] - 1];
             brokers.length--;
             brokerMap[_broker] = 0;
         } else {
@@ -42,7 +51,7 @@ contract Crowdfunding is Ownable {
 
     function addOrgToWhitelist(address _org) public {
         if (organizationWhitelist[_org] == 0) {
-            organizationWhitelist[_org] = organizations.length;
+            organizationWhitelist[_org] = organizations.length + 1;
             organizations.push(_org);
         } else {
             revert('Already on whitelist');
@@ -51,7 +60,7 @@ contract Crowdfunding is Ownable {
 
     function removeOrgFromWhitelist(address _org) public {
         if (organizationWhitelist[_org] != 0) {
-            delete organizations[organizationWhitelist[_org]];
+            delete organizations[organizationWhitelist[_org] - 1];
             organizations.length--;
             organizationWhitelist[_org] = 0;
         } else {
@@ -61,7 +70,7 @@ contract Crowdfunding is Ownable {
 
     function addUserToWhitelist(address _user) public {
         if (userWhitelist[_user] == 0) {
-            userWhitelist[_user] = users.length;
+            userWhitelist[_user] = users.length + 1;
             users.push(_user);
         } else {
             revert('Already on whitelist');
@@ -70,7 +79,7 @@ contract Crowdfunding is Ownable {
 
     function removeUserFromWhitelist(address _user) public {
         if (userWhitelist[_user] != 0) {
-            delete users[userWhitelist[_user]];
+            delete users[userWhitelist[_user] - 1];
             users.length--;
             userWhitelist[_user] = 0;
         } else {
