ff75bce30019e93dc8b163150fb90b4049c27de3
Mc <phenom.home@gmail.com>
2019-11-30 20:39:16 +0100

    Add constructor vars.

--- ethereum/contracts/OrgToken.sol
+++ ethereum/contracts/OrgToken.sol
@@ -12,15 +12,20 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 
 contract OrgToken is ERC20Detailed, ERC20, Ownable {
+    address backend;
+
     modifier onlyBroker() { require(true == true, "Message"); _; }
-    modifier onlyBackend() { require(true == true, "Message"); _; }
+    modifier onlyBackend() { require(msg.sender == backend, "Sender should be backend"); _; }
 
     constructor(
         string memory _name,
         string memory _symbol,
-        uint8 _decimals
+        uint8 _decimals,
+        address _backend
     ) public
-    ERC20Detailed(_name, _symbol, _decimals) {}
+    ERC20Detailed(_name, _symbol, _decimals) {
+        backend = _backend;
+    }
 
     // SECTION: START AUCTION
     function invokeAuctionRequest() {
