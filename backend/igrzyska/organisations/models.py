7bbd4ed9a13c9cde6f9666d6a20447d9a4758dbb
mkryna <maciej.krynski@tylko.com>
2019-11-30 15:40:09 +0100

    created organisation model

--- backend/igrzyska/organisations/models.py
+++ backend/igrzyska/organisations/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Organisation(models.Model):
+    name = models.CharField(max_length=64)
+    public_key = models.CharField(max_length=128)
