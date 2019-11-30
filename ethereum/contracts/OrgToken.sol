1a22db32e4d4920759b2a59535e7fd111da30afe
Mc <phenom.home@gmail.com>
2019-11-30 20:32:07 +0100

    Eth stubs.

--- ethereum/contracts/OrgToken.sol
+++ ethereum/contracts/OrgToken.sol
@@ -12,10 +12,60 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 
 contract OrgToken is ERC20Detailed, ERC20, Ownable {
+    modifier onlyBroker() { require(true == true, "Message"); _; }
+    modifier onlyBackend() { require(true == true, "Message"); _; }
+
     constructor(
         string memory _name,
         string memory _symbol,
         uint8 _decimals
     ) public
     ERC20Detailed(_name, _symbol, _decimals) {}
+
+    // SECTION: START AUCTION
+    function invokeAuctionRequest() {
+        // invoke auction request by comp/org
+    }
+
+    function signAuctionRequest()
+    onlyBroker() {
+        // sign auction request by one of brokers
+    }
+
+    function startAuction() {
+        // start auction by company/org when auction is signed by comp/org
+    }
+
+    // SECTION: INVEST
+    function becomeInvestorRequest() {
+        // investor request by user
+    }
+
+    function signInvestorRequest()
+    onlyBroker() {
+        // sign investor request by one of brokers
+    }
+
+    function buildInvestInAuction(address _from, uint256 _value)
+    onlyBackend()
+    returns (bytes32) {
+        // build invest method for user by backend
+        return 0x0;
+    }
+
+    function investInAuction(bytes32 _message, uint8 _v, bytes32 _r, bytes32 _s) payable {
+        // investing in auction by user
+        address signer = ecrecover(_message, _v, _r, _s);
+    }
+
+    // SECTION: CONFIGURE BROKER
+    function addBroker(address _broker)
+    onlyOwner() {
+        // add broker by owner
+    }
+
+    function removeBroker(address _broker)
+    onlyOwner() {
+        // remove broker by owner
+    }
 }
