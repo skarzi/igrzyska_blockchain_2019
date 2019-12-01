a453263ffb4a2c00cda3c167782cdca6787502c8
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2019-12-01 12:44:17 +0100

    next iteration of intrgration with blockchain

---
+++ backend/igrzyska/web3_utils.py
@@ -0,0 +1,8 @@
+from web3 import Web3
+
+public_w3 = Web3(Web3.HTTPProvider("http://eth:8546"))
+private_w3 = Web3(Web3.HTTPProvider("http://quorum:8547"))
+
+
+def create_organisation(organisation):
+    pass
