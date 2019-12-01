e9e8a8c9cc1d36cbd3ec5095d1f88af5f56a5c0d
Mc <phenom.home@gmail.com>
2019-12-01 13:18:11 +0100

    Add all keys.

--- ethereum/migrations/config/global.js
+++ ethereum/migrations/config/global.js
@@ -5,10 +5,10 @@ DotEnv.config();
 class GlobalConfig {
     constructor() {
         this.keepDeployedFile = process.env.KEEP_DEPLOYED;
-        this.backend = '0xF7DE62B65768a169279be74b12FaA65a22FB38D3';
-        this.broker = '0xF7DE62B65768a169279be74b12FaA65a22FB38D3';
-        this.organization = '0xF7DE62B65768a169279be74b12FaA65a22FB38D3';
-        this.user = '0xF7DE62B65768a169279be74b12FaA65a22FB38D3';
+        this.backend = '0x3D4A893c5f26E9b3200AffA75aDfd1492932B5d8';
+        this.broker = '0xe625Ea79101f0f05C55f4134cEc71628016717b8';
+        this.organization = '0x235bFCb8B953538d70b806CF475851a520b980ca';
+        this.user = '0x9B2daa270735ccf956e7112952C2C5ABf423f4aE';
         this.tokenName = 'TestAuction';
         this.tokenSymbol = 'TA';
         this.supply = 1 * 10**6;
